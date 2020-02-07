a4f0c2089a5a287a4ee7c006e016f49a689c440b
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2020-02-07 02:40:32 +0000

    Update docs after building Travis build 20 of QAlgebra/qalgebra
    The docs were built from the branch 'master' against the commit
    31247ccb264098b4892f560bb09211fd4068887f.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/QAlgebra/qalgebra/jobs/647153517.
    
    The doctr command that was run is
    
        /home/travis/virtualenv/python3.8.0/lib/python3.8/site-packages/doctr/__main__.py deploy --key-path docs/doctr_deploy_key.enc '--command=git show 31247ccb264098b4892f560bb09211fd4068887f:.travis/doctr_post_process.py > post_process.py && git show 31247ccb264098b4892f560bb09211fd4068887f:.travis/versions.py > versions.py && python post_process.py' --built-docs docs/_build/html --no-require-master --build-tags master

--- master/_static/documentation_options.js
+++ master/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2.0.0-dev (a06528e)',
+    VERSION: '2.0.0-dev (31247cc)',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
